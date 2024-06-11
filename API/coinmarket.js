1b52b3a563668dccf4f694a2910e3e50a5362848
nandhuraju <nandhuraju6@gmail.com>
2024-06-11 11:33:12 +0530

    update

--- API/coinmarket.js
+++ API/coinmarket.js
@@ -1,4 +1,4 @@
-//3ff420c8-31a5-441d-8be6-18b67598b08e
+
 
 async function AsyFn()
 { let x=await fetch("")
@@ -7,8 +7,6 @@ async function AsyFn()
     console.log(y)
 
    
-    // await(response=>response.json())
-    // (json=>console.log(json))
- 
+    
 }
 AsyFn()
